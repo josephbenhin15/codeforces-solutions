49bdc5802f3a127d96bd42ad6d435346e7d1bc61
josephbenhin15 <josephdontheboina@gmail.com>
2026-06-24 13:02:30 +0530

    Sync Codeforces: 2065B - Скибидус и Огайо [C++23 (GCC 14-64, msys2)]

---
+++ 2065B_.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+ 
+void josben() {
+    string str;
+    cin >> str;
+ 
+    for (int i = 1; i < str.size(); i++) {
+        if (str[i - 1] == str[i]) {
+            cout << 1 << '\n';
+            return;
+        }
+    }
+ 
+    cout << str.size() << '\n';
+}
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        josben();
+    }
+    return 0;
+}
