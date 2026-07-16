67cac89418d9544a175e674ebd76b62b3410e4cb
josephbenhin15 <josephdontheboina@gmail.com>
2026-07-16 22:46:31 +0530

    Sync Codeforces: 2092A - Камилька и барашки [C++23 (GCC 14-64, msys2)]

---
+++ 2092A_.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        for(auto&x:a)cin>>x;
+        sort(a.begin(),a.end());
+        int mx=*max_element(a.begin(),a.end());
+        int mn=*min_element(a.begin(),a.end());
+        cout<<mx-mn<<endl;
+    } 
+}
