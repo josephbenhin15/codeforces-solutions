430776a9a303bca98a903565d2cf4c356fe5d9ad
josephbenhin15 <josephdontheboina@gmail.com>
2026-08-01 09:29:31 +0530

    Sync Codeforces: 2090A - Поиск клада [C++20 (GCC 13-64)]

---
+++ 2090A_.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y,a;
+        cin>>x>>y>>a;
+       if (a % (x + y) < x) {
+        cout << "NO\n";
+    } else {
+        cout << "YES\n";
+    }
+    }
+    return 0;
+} 
