4b08ffc3521fad1df898735419addf8606a7feb7
josephbenhin15 <josephdontheboina@gmail.com>
2026-08-24 22:19:31 +0530

    Sync Codeforces: 2134B - Добавить 0 или K [C++20 (GCC 13-64)]

---
+++ 2134B_0_K.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, k;
+    cin >> n >> k;
+    vector<long long> a(n);
+    for (auto &i : a) {
+      cin >> i;
+      i += (i % (k + 1)) * k;
+    }
+    for (auto i : a)
+      cout << i << ' ';
+    cout << '\n';
+  }
+}
